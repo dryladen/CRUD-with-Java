be8519a35962db6f479b7357fb3177b6e56645bd
dryladen <delfanladen24@gmail.com>
2022-02-22 16:05:53 +0800

    Base code

---
+++ src/crud/Item.java
@@ -0,0 +1,42 @@
+package crud;
+
+public class Item {
+    private String name;
+    private float price;
+    private int stock;
+
+    public Item() {
+
+    }
+
+    public Item(String name, float price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+}
---
+++ src/crud/Main.java
@@ -0,0 +1,16 @@
+package crud;
+
+/**
+ *
+ * @author Laden
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
