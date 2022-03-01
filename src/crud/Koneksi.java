f1eba65eebb8d53f50263a765f8ec5772f84c057
dryladen <delfanladen24@gmail.com>
2022-03-01 19:37:35 +0800

    Add database

---
+++ src/crud/Koneksi.java
@@ -0,0 +1,117 @@
+package crud;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import java.io.File;
+
+public class Koneksi {
+    private final String url = "jdbc:sqlite:databaseitem.db";
+    private PreparedStatement pst;
+    private Connection koneksi;
+    private ResultSet result;
+    private Statement stm;
+    private String sql;
+
+    public boolean isDatabaseExists(String dbFilePath) {
+        File database = new File(dbFilePath);
+        return database.exists();
+    }
+
+    public Connection getKoneksi() { // menghubungkan ke database
+        String getFilePath = new File("").getAbsolutePath();
+        String fileAbsolutePath = getFilePath.concat("\\databaseitem.db");
+        if (isDatabaseExists(fileAbsolutePath)) {
+            try {
+                koneksi = DriverManager.getConnection(url);
+            } catch (SQLException ex) {
+                Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, ex);
+                JOptionPane.showMessageDialog(null, "Error koneksi database : " + ex);
+            }
+        }
+        // } else {
+        // createNewDatabase();
+        // try {
+        // koneksi = DriverManager.getConnection(url);
+        // } catch (SQLException ex) {
+        // Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, ex);
+        // JOptionPane.showMessageDialog(null, "Error create database : "+ ex);
+        // }
+        // }
+        return koneksi;
+    }
+
+    public ArrayList<Item> getData(ArrayList<Item> item) {
+        try {
+            Connection cn = getKoneksi();
+            stm = cn.createStatement();
+            result = stm.executeQuery("SELECT * FROM Item");
+            while (result.next()) {
+                item.add(new Item(result.getString(1), result.getFloat(2), result.getInt(3)));
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error : " + ex);
+        } finally {
+            try {
+                result.close();
+                stm.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error set data : " + ex);
+            }
+        }
+        return item;
+    }
+    // private void createNewDatabase(){
+    // Connection conn;
+    // try {
+    // Class.forName("org.sqlite.JDBC");
+    // conn = DriverManager.getConnection(url);
+    // if(conn != null){
+    // try {
+    // sql = "CREATE TABLE \"kategoriTable\" (\n" +
+    // " \"idKategori\" INTEGER NOT NULL,\n" +
+    // " \"namaKategori\" TEXT DEFAULT NULL,\n" +
+    // " \"tanggalKategori\" TEXT DEFAULT NULL,\n" +
+    // " \"deskripsiKategori\" TEXT DEFAULT NULL,\n" +
+    // " PRIMARY KEY(\"idKategori\" AUTOINCREMENT)\n" +
+    // ")";
+    // Statement kategoriTable = conn.createStatement();
+    // kategoriTable.execute(sql);
+    // sql = "CREATE TABLE \"kegiatanTable\" (\n" +
+    // " \"idKegiatan\" INTEGER NOT NULL,\n" +
+    // " \"namaKegiatan\" TEXT DEFAULT NULL,\n" +
+    // " \"tanggalKegiatan\" TEXT DEFAULT NULL,\n" +
+    // " \"deskripsiKegiatan\" TEXT DEFAULT NULL,\n" +
+    // " \"idKategori\" INTEGER NOT NULL,\n" +
+    // " \"isCheck\" INTEGER DEFAULT 0,\n" +
+    // " FOREIGN KEY(\"idKategori\") REFERENCES
+    // \"kategoriTable\"(\"idKategori\"),\n" +
+    // " PRIMARY KEY(\"idKegiatan\" AUTOINCREMENT)\n" +
+    // ")";
+    // Statement kegiatanTable = conn.createStatement();
+    // kegiatanTable.execute(sql);
+    // sql = "CREATE TABLE \"lokasiTable\" (\n" +
+    // " \"getX\" INTEGER DEFAULT NULL,\n" +
+    // " \"getY\" INTEGER DEFAULT NULL,\n" +
+    // " \"width\" INTEGER DEFAULT NULL,\n" +
+    // " \"height\" INTEGER DEFAULT NULL\n" +
+    // ")";
+    // Statement lokasiTable = conn.createStatement();
+    // lokasiTable.execute(sql);
+    // sql = "INSERT INTO lokasiTable (getX,getY,width,height) VALUES
+    // (0,0,312,386)";
+    // PreparedStatement pst = conn.prepareStatement(sql);
+    // pst.execute();
+    // } catch (SQLException ex){
+    // Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, ex);
+    // JOptionPane.showMessageDialog(null, "Create database error : "+ ex);
+    // }
+    // }
+    // } catch (SQLException | ClassNotFoundException ex) {
+    // Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, ex);
+    // JOptionPane.showMessageDialog(null, "Create databases error : "+ ex);
+    // }
+    // }
+}
--- src/crud/Main.java
+++ src/crud/Main.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Laden
@@ -15,10 +21,24 @@ public class Main {
      * @param args the command line arguments
      * @throws IOException
      */
+
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Item> dataItem = new ArrayList<Item>();
+        Koneksi koneksi = new Koneksi();
+        JFrame frameTable = new JFrame("List Item");
+        String itemSearch;
         String selectMenu = "0";
+        String header[] = { "Name", "Price", "Amount" };
+        JTable table = new JTable();
+        DefaultTableModel tblModel = new DefaultTableModel();
+        tblModel.setColumnIdentifiers(header);
+        table.setModel(tblModel);
+        ArrayList<Item> dataItem = new ArrayList<Item>();
+        JScrollPane data = new JScrollPane(table);
+        koneksi.getData(dataItem);
+        for (Item item : dataItem) {
+            tblModel.addRow(new Object[] { item.getName(), item.getPrice(), item.getAmount() });
+        }
         while (!selectMenu.equals("6")) {
             System.out.println("Welcome to CRUD program");
             System.out.println(">> MENU <<");
@@ -33,7 +53,6 @@ public class Main {
             switch (selectMenu) {
                 case "1":
                     try {
-
                         Item newItem = new Item();
                         System.out.print("Input Name   : ");
                         newItem.setName(input.readLine());
@@ -42,31 +61,46 @@ public class Main {
                         System.out.print("Input Amount : ");
                         newItem.setAmount(Integer.parseInt(input.readLine()));
                         dataItem.add(newItem);
+                        tblModel.addRow(new Object[] { newItem.getName(), newItem.getPrice(), newItem.getAmount() });
                     } catch (NumberFormatException e) {
                         System.out.println("Input Error !!");
                     }
                     break;
                 case "2":
-                    System.out.println("Menu 2");
+                    System.out.print("Masukan nama item : ");
+                    itemSearch = input.readLine();
+                    for (Item item : dataItem) {
+                        if (item.getName().equals(itemSearch)) {
+                            System.out.print("Input new Price  : ");
+                            item.setPrice(Float.parseFloat(input.readLine()));
+                            System.out.print("Input new Amount : ");
+                            item.setAmount(Integer.parseInt(input.readLine()));
+                            System.out.println("Update Complete");
+                            break;
+                        }
+                    }
                     break;
                 case "3":
-                    System.out.println("Menu 3");
+                    System.out.print("Masukan nama item : ");
+                    itemSearch = input.readLine();
+                    for (int i = 0; i < dataItem.size(); i -= -1) {
+                        if (dataItem.get(i).getName().equals(itemSearch)) {
+                            dataItem.remove(i);
+                            System.out.println("Delete Complete");
+                            break;
+                        }
+                    }
                     break;
                 case "4":
-                    int i = 1;
-                    for (Item item : dataItem) {
-                        System.out.println("Item " + i);
-                        System.out.println("Name   : " + item.getName());
-                        System.out.println("Price  : " + item.getPrice());
-                        System.out.println("Amount : " + item.getAmount());
-                        System.out.println();
-                        i -= -1;
-                    }
+                    frameTable.add(data);
+                    frameTable.setBounds(400, 100, 600, 220);
+                    frameTable.setVisible(true);
                     break;
                 case "5":
                     System.out.println("Menu 5");
                     break;
                 case "6":
+                    frameTable.dispose();
                     break;
                 default:
                     System.out.println(">> Wrong input <<");
